a01bbac787338e32a542ae38b9c1701a5d7e4171
jrf115 <jrf115@zips.uakron.edu>
2020-03-08 18:51:05 -0400

    Add logistic reg confusion matrix

--- logistic.py
+++ logistic.py
@@ -13,7 +13,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
 
 def _logistic(X, y, lookup_fall_type):
-    print("__logistic regression__")
+    print("\n__logistic regression__")
     # random_state: set seed for random# generator
     # test_size: default 25% testing, 75% training
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=42)
@@ -35,3 +35,10 @@ def _logistic(X, y, lookup_fall_type):
     # first example: a small fruit with mass 20g, color_score = 5.5, width 4.3 cm, height 5.5 cm
     fall_prediction = lr.predict([[4722.92, 4019, -1.61E+03, 14, 78, 319]])
     print(lookup_fall_type[fall_prediction[0]])
+
+    # Estimate the accuracy of the classifier on future data, using the test data
+    lr.score(X_test, y_test)
+    lr_predict = lr.predict(X_test)
+    print("The prediction", lr_predict)
+    print("Logistic Regression Confusion Matrix:\n")
+    print(confusion_matrix(y_test, lr_predict))
